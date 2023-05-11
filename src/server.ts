5c3366495c9dae6e947b6f1f051721fea95cacd3
Aleksandar Vasic <adrijana.work@gmail.com>
2023-05-11 22:06:40 +0200

    Fix

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { authMiddleware } from "./utils/index.js";
 const app = express();
 
 const corsOptions = {
-  origin: process.env.API,
+  origin: ['https://vtspace-cow4.onrender.com'],
   optionsSuccessStatus: 200,
   credentials: true,
 };
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", process.env.API);
+  res.header("Access-Control-Allow-Origin", 'https://vtspace-cow4.onrender.com');
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
